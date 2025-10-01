c6bf55e08165206bd2feaad0a9ba310ac43416da
Shin Hakuro <muhammadrizky040206@gmail.com>
2025-10-01 15:09:32 +0700

    Add initial navbar styles

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <div class="nav-container">
+        <div class="nav-logo">
+            <a href=".php">My E-Commerce</a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="add_product.php">Add Produk</a></li>
+            <li><a href="view_products.php">View Produk</a></li>
+        </ul>
+    </div>
+</nav>
