ef71d4760335c68a28a66036be3064209a317b2d
LennoNardo <nardolenno@gmail.com>
2025-10-01 14:56:16 +0700

    Update header title, adjust container width in table styles, and add navbar inclusion

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>E-Commerce</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="add.css">
     <link rel="stylesheet" href="table.css">
 </head>
-
+    <?php include 'navbar.php' ?>
 <body>
